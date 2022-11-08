57a9d890dcbabed4841e9a1b276ff17fdc7c7006
sushmaprakash <sushmaprakash353@gmail.com>
2022-11-08 15:11:09 -0800

    d

---
+++ Desktop/sk/mat.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 03:05:14 2022
+
+@author: SPTINT-13
+"""
+
+import numpy as np
+
+arr=np.array([3,4,6])
+
+
+arr=np.array([3,5,6]),([7,9,10])
+print(arr)
+
+
+x=np.array([1,2,3,4,5,6,7,8,9])
+arr=x.reshape(3,3)
+
+
+arr=np.zeros([3,3])
+print(arr+1)
+
+import numpy as np
+arr=np.array([1,2,3])
+print(arr+1)
+print(arr-1)
+print(arr*1)
+print(arr/1)
+
+
